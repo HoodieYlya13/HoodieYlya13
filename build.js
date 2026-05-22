afc8a5bb1be01c78dd894aaf334f022af44a0351
Ylya <ylyamartchenko@gmail.com>
2026-05-22 15:35:58 +0200

    logo path update

--- build.js
+++ build.js
@@ -70,7 +70,7 @@ const formattedTraits = `${traits
 const readmeContent = `
 <div align="center">
   <a href="${profile.communication.links.live_portfolio}">
-    <img src="https://www.hy13dev.com/img/logo.png" alt="Ylya Martchenko Logo" width="150" />
+    <img src="https://www.hy13dev.com/logo.png" alt="Ylya Martchenko Logo" width="150" />
   </a>
 
   <h1>Hi there, I'm ${profile.identity.full_name}! 👋</h1>
